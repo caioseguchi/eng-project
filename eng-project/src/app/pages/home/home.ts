8c163b615f4582a2215299edeb031e9da53bf8f6
caioseguchi <caioseguchi@gmail.com>
2025-12-02 20:31:42 +0000

    Add frontend home page and header component

--- eng-project/src/app/pages/home/home.ts
+++ eng-project/src/app/pages/home/home.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { Header } from '../../components/header/header';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [Header],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
-
-}
+export class Home {}
